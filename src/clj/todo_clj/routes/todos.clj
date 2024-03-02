f90d5ad71a88d82bf4989cce8e2ac634a478644d
Brenno Rodrigues <brennopereira6@gmail.com>
2024-03-02 03:13:11 +0000

    feat; to-do routes

---
+++ src/clj/todo_clj/routes/todos.clj
@@ -0,0 +1,30 @@
+(ns todo-clj.routes.todos
+  (:require
+   [todo-clj.config :refer [env]]
+   [todo-clj.middleware :as middleware]
+   [ring.util.response]
+   [ring.util.http-response :as response]
+   [buddy.sign.jwt :as jwt]
+   [todo-clj.db.todos :as todos]))
+
+(defn index-page [request]
+  (let [db (env :database-options)
+        token (get (:headers request) "authorization")
+        payload (jwt/unsign token "test-key")
+        searchParams (:body-params request)
+        result (todos/select-todos db (assoc searchParams :user_id (:id (:user payload))))]
+    (response/ok {:todos result})))
+
+(defn create-page [request]
+  (let [db (env :database-options)
+        token (get (:headers request) "authorization")
+        payload (jwt/unsign token "test-key")
+        userid (:id (:user payload))
+        body (:body-params request)
+        result (first (todos/insert-todo db (assoc body :user_id userid)))]
+    (response/ok {:todo result})))
+
+(defn todos-routes []
+  [""
+   {:middleware [middleware/wrap-formats]}
+   ["/todos/" {:get index-page :post create-page}]])
