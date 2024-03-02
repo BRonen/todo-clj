ec41339d4ae655c36e984cefddc28969db5ae63f
Brenno Rodrigues <brennopereira6@gmail.com>
2024-03-02 05:18:12 +0000

    refactor: removing unused route

--- src/clj/todo_clj/routes/home.clj
+++ src/clj/todo_clj/routes/home.clj
@@ -1,18 +1,9 @@
 (ns todo-clj.routes.home
   (:require
-   [todo-clj.config :refer [env]]
    [todo-clj.layout :as layout]
    [clojure.java.io :as io]
    [todo-clj.middleware :as middleware]
-   [ring.util.response]
-   [ring.util.http-response :as response]
-   [todo-clj.db.users :as users]))
-
-(defn tttt-page [request]
-  (println request)
-  (let [db (env :database-url)]
-    (println (users/insert-user db {:username "hello" :password "worldpass"})))
-  (response/ok {:foo "bar"}))
+   [ring.util.response]))
 
 (defn home-page [request]
   (layout/render request "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
@@ -25,6 +16,5 @@
    {:middleware [middleware/wrap-csrf
                  middleware/wrap-formats]}
    ["/" {:get home-page}]
-   ["/wasd" {:get tttt-page}]
    ["/about" {:get about-page}]])
 
